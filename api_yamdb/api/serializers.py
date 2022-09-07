1a5d1fed84438ca86786360e7721b2a0ad09f577
Alexey Petukhov <69753101+memphis94@users.noreply.github.com>
2022-09-07 15:14:35 +0300

    merge

---
+++ api_yamdb/api/serializers.py
@@ -0,0 +1,28 @@
+from rest_framework import serializers
+from reviews.models import Comment, Review
+
+
+
+
+
+class CommentSerializer(serializers.ModelSerializer):    
+    author = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field='username'
+    )
+
+    class Meta:
+        model = Comment
+        fields = '__all__'
+        read_only_fields = ('review', )
+
+
+class ReviewSerializer(serializers.ModelSerializer):    
+    author = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field='username'
+    )
+
+    class Meta:
+        model = Review
+        fields = ('id', 'text', 'author', 'score', 'pub_date')
