2010f6e74c358f1c2018a6a772ce8149de4aad24
Alexey Petukhov <69753101+memphis94@users.noreply.github.com>
2022-09-08 23:53:34 +0300

    Добавил пермишены для Комментов и отзывов, вьюсеты и url

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,13 +1,21 @@
 from rest_framework.routers import DefaultRouter
 from django.urls import include, path
 
-from .views import (CategoriesViewSet, GenresViewSet, TitlesViewSet)
+from .views import (CategoriesViewSet, CommentViewSet, GenresViewSet, ReviewViewSet, TitlesViewSet)
 
 router_v1 = DefaultRouter()
 router_v1.register('titles', TitlesViewSet, basename='title')
 router_v1.register('categories', CategoriesViewSet, basename='category')
 router_v1.register('genres', GenresViewSet, basename='genre')
+router_v1.register(
+    r'titles/(?P<title_id>\d+)/reviews', views.ReviewViewSet, basename='review'
+)
+router_v1.register(
+    r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
+    views.CommentViewSet,
+    basename='comment',
+)
 
 urlpatterns = [
-    path('v1/', include(router_v1.urls)),
+    path('v1/', include(router_v1.urls)),    
 ]
