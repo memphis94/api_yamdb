1ecc0e5990ddc4b054343465a7725b1ae9f18f4a
Alexey Petukhov <69753101+memphis94@users.noreply.github.com>
2022-09-12 21:05:45 +0300

    Добавлен валидатор в сериализатор для Review, добавлено ограничение в модели Review

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,7 +1,8 @@
 from rest_framework import serializers
 from reviews.models import Category, Genre, Title
 from reviews.models import Comment, Review
-
+from django.core.exceptions import ValidationError
+from django.shortcuts import get_object_or_404
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
@@ -61,9 +62,26 @@ class ReviewSerializer(serializers.ModelSerializer):
         read_only=True,
         slug_field='username'
     )
+    title = serializers.SlugRelatedField(
+        slug_field='name',
+        read_only=True
+    )
+
+    def validate(self, data):
+        request = self.context['request']
+        author = request.user
+        title_id = self.context.get('view').kwargs.get('title_id')
+        title = get_object_or_404(Title, pk=title_id)
+        if (
+            request.method == 'POST'
+            and Review.objects.filter(title=title, author=author).exists()
+        ):
+            raise ValidationError("Можно добавить только один отзыв")
+        return data
 
     class Meta:
+        fields = '__all__'
         model = Review
-        fields = ('id', 'text', 'author', 'score', 'pub_date')
+    
 
         
